4aa19c612f6ac4de46817a8ba69404dc28c2f432
Dhruva062 <djnath0011@gmail.com>
2026-06-08 12:15:42 +0530

    Remove convert_logo.py

--- convert_logo.py
+++
@@ -1,5 +0,0 @@
-# run this once: convert_logo.py
-from PIL import Image
-img = Image.open("logo.png")
-img.save("logo.ico", format="ICO", sizes=[(256,256),(128,128),(64,64),(48,48),(32,32),(16,16)])
-print("logo.ico created!")
